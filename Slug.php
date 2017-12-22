910e4d4a77ad34708c409bed15118cfe3239deb3
Alex <32906781+alex74m@users.noreply.github.com>
2017-12-22 13:38:14 +0100

    Create Slug.php

---
+++ Slug.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services;
+use \App\Repository\DbRequest;
+
+class Slug
+{
+	public static function slugify($text)
+	{
+		$text = preg_replace('~[^\pL\d]+~u', '-', $text);
+		$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+		$text = preg_replace('~[^-\w]+~', '', $text);
+		$text = trim($text, '-');
+		$text = preg_replace('~-+~', '-', $text);
+		$text = strtolower($text);
+		$text = str_replace(' ', '-', $text);
+		return $text;
+	}
+	public static function createSlug($text)
+	{
+		$text = self::slugify($text);
+		$lastChar = substr($text, -1, 1);
+		settype($lastChar, 'integer');
+		if (!is_numeric($lastChar)) {
+		  	$text = $text.'-'.'1';  
+		}else{
+			if (substr($text, -2, 2) == ('-'.$lastChar)) {
+				$text = rtrim($text, '-'.$lastChar); 
+			}
+		   	$lastChar = $lastChar + 1;
+		  	$text = $text.'-'.$lastChar;  
+		}
+		return $text;
+	}
+}
+
+/*
+ * Use App\Services\Slug
+ * Set title slugyfication
+ * return string
+ * @param string 
+ */
+
+$titre = $post['_sTitre'];
+
+$slugName = Slug::slugify($titre);
+
+$vérificationIfExistInBdd = 'SELECT ... FROM table WHERE ...';
+
+
+$checkSlug = $vérificationIfExistInBdd('article', 'art_sSlug', $slugName);
+
+if ($checkSlug === false) {
+	$slugName = Slug::createSlug($titre);
+}
